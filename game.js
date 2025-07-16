40b3d78bd2023fa0c1b48ca8e3e849e136b34f04
Marcel Ukeje <ukejechibundu@gmail.com>
2025-07-16 09:02:51 +0100

    Make final improvements to the game.

--- game.js
+++ game.js
@@ -1,89 +1,64 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-
-    if(choice === 0){
-        option = "rock";
-    }else if(choice === 1){
-        option = "paper";
-    }else {
-        option = "scissors";
-    }
-
-    return option;
+    let options = ["rock", "paper", "scissors"];
+    return options[choice];
 }
 
 function getHumanChoice() {
     let humanOption = prompt("Enter your option! (Rock, Paper, or Scissors?)");
 
-    humanOption = humanOption.toLowerCase();
-
-    return humanOption;
+    return humanOption?.trim().toLowerCase();
 }
 
 
-humanScore = 0;
-computerScore = 0;
 
-// humanChoice = getHumanChoice()
-// computerChoice = getComputerChoice()
 
-function playRound(humanChoice=getHumanChoice(), computerChoice=getComputerChoice()) {
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
 
-    switch (`${humanChoice}-${computerChoice}`){
-        case "rock-rock":
-            console.log("It's a tie!");
-            break;
+    function playRound(humanChoice, computerChoice) {
+        switch (`${humanChoice}-${computerChoice}`){
+            case "rock-rock":
+            case "paper-paper":
+            case "scissors-scissors":
+                console.log("It's a tie!");
+                console.log(`Current scores: Human ${humanScore}, Computer ${computerScore}`);
+                return "It's a tie!";
 
-        case "rock-paper":
-            computerScore++
-            console.log("You lose! Paper beats Rock.");
-            break;
+            case "rock-paper":
+            case "paper-scissors":
+            case "scissors-rock":
+                computerScore++
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+                console.log(`Current scores: Human ${humanScore}, Computer ${computerScore}`);
+                return `You lose! ${computerChoice} beats ${humanChoice}.`;
 
-        case "rock-scissors":
-            humanScore++
-            console.log("You win! Rock beats Scissors.");
-            break;
-    
-        case "paper-rock":
-            humanScore++
-            console.log("You win! Paper beats Rock.");
-            break;
-
-        case "paper-paper":
-            console.log("It's a tie!");
-            break;
-
-        case "paper-scissors":
-            computerScore++
-            console.log("You lose! Scissors beats Paper.");
-            break;
-
-        case "scissors-rock":
-            computerScore++
-            console.log("You lose! Rock beats Scissors.");
-            break;
-
-        case "scissors-paper":
-            humanScore++
-            console.log("You win! Scissors beats Paper.");
-            break;
-
-        case "scissors-scissors":
-            console.log("It's a tie!");
-            break;
+            case "rock-scissors":
+            case "paper-rock":
+            case "scissors-paper":
+                humanScore++
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                console.log(`Current scores: Human ${humanScore}, Computer ${computerScore}`);
+                return `You win! ${humanChoice} beats ${computerChoice}.`;
+        }
     }
-    return (`Your score: ${humanScore}, Computer's score: ${computerScore}`);
+    
+    for(let i = 0; i < 5; i++){
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        
+        playRound(humanChoice, computerChoice);
+    }
+
+    if (humanScore > computerScore){
+         console.log(`You won! And you did that with ${humanScore} points against ${computerScore} points!`)
+    }else if (humanScore < computerScore){
+         console.log(`You lost! The computer beat you with ${computerScore} points against ${humanScore} points!`)
+    }else {
+         console.log(`It's a tie. You both got ${humanScore} points!`)
+    }
 }
 
+playGame();
 
-function playGame(start){
-    if (start === true){
-        console.log(playRound());
-        console.log(playRound());
-        console.log(playRound());
-        console.log(playRound());
-        console.log(playRound());
-    }   
-}
-
-playGame(true)
